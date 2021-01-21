fe1828de83cff63c668c4c3187b5d37fb2260375
surjeet singh <surjeetsingh01010@gmail.com>
2021-01-21 12:26:24 +0530

    first version of keeper app

---
+++ src/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note(){
+    return(
+        <div className="note">
+            <h1>
+                this is note title
+            </h1>
+            <p>
+                this is the note details mentioned as the paragraph
+            </p>
+        </div>
+    );
+} 
+
+export default Note;
