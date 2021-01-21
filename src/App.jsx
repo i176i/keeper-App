b427530897a6ca217a2263953bea84d616166c41
surjeet singh <surjeetsingh01010@gmail.com>
2021-01-21 13:20:12 +0530

    vesion 2.0.0, update with the props and map functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "./notes"
+
+function newNote(oneNote){
+    return(
+        <Note 
+        title = {oneNote.title}
+        content = {oneNote.content}
+        />
+    )
+}
 
 function App(){
     return(
         <div>
             <Header />
-            <Note />
+            {notes.map(newNote)}
             <Footer />
 
         </div>
--- src/Note.jsx
+++ src/Note.jsx
@@ -1,16 +1,18 @@
+
 import React from "react";
 
-function Note(){
+function Note(props){
     return(
         <div className="note">
             <h1>
-                this is note title
+                {props.title}
             </h1>
             <p>
-                this is the note details mentioned as the paragraph
+                {props.content}
             </p>
         </div>
     );
 } 
+
 
 export default Note;
